0fa303caa890c404f497e6f6db34b36467d1486c
Hasturas <liyuefan@sjtu.edu.cn>
2024-09-25 08:25:05 +0800

    model Lyf

---
+++ Security/Lyf_Logs/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name="Lyf_Logs"
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("add/", views.add, name="add"),
+    path("delete/<int:id>/", views.delete, name="delete"),
+    path("edit/<int:id>/", views.edit, name="edit"),
+    path("view/<int:id>/", views.view, name="view"),
+]
--- Security/Lyf_Logs/views.py
+++ Security/Lyf_Logs/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def add(request):
+    return HttpResponse("You're adding a new log entry.")
+
+def delete(request, id):
+    return HttpResponse(f"You're deleting log entry {id}.")
+
+def edit(request, id):
+    return HttpResponse(f"You're editing log entry {id}.")
+
+def view(request, id):
+    return HttpResponse(f"You're viewing log entry {id}.")
+
