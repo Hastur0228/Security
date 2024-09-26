173966ca81c22c2dc668e2b0da171a466be8bc18
Hasturas <liyuefan@sjtu.edu.cn>
2024-09-26 10:52:39 +0800

    allow hosts

--- Security/Lyf_Logs/urls.py
+++ Security/Lyf_Logs/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name="Lyf_Logs"
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.lyf, name="Lyf"),
     path("add/", views.add, name="add"),
     path("delete/<int:id>/", views.delete, name="delete"),
     path("edit/<int:id>/", views.edit, name="edit"),
--- Security/Lyf_Logs/views.py
+++ Security/Lyf_Logs/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import LyfLogs
 
 # Create your views here.
-def index(request):
-    return render(request, "index.html")
+def lyf(request):
+    latest_lyf_list = LyfLogs.objects.order_by("-created_at")[:5]
+    context = {"latest_lyf_list": latest_lyf_list}
+    return render(request, "lyf.html", context)
 
 def add(request):
     return HttpResponse("You're adding a new log entry.")
